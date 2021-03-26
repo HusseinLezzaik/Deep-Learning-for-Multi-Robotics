cd5ee77704fa2af388a693a177b196f43806f7f8
hussein <hsenlezaik@gmail.com>
2021-03-27 00:12:06 +0100

    Pose Transformer

--- Pose Transformer/Transformation_code.py
+++ Pose Transformer/Transformation_code.py
@@ -1,32 +1,40 @@
 import math
 import numpy as np
 
-k = 1
-L = 1
-d = 1
-v1 = 0
-w1 = 0
-v2 = 0
-w2 = 0
-vL1 = 0
-vR1 = 0
-vL2 = 0 
-vR2 = 0
 
 " Calculate Control inputs u1 and u2 "
-u1 = np.array([[ k*(x2-x1) ], [k*(y2-y1)]])
-u2 = np.array([[ k*(x1-x2) ], [k*(y1-y2)]])
-
+            
+u1 = np.array([[ k*(self.x2-self.x1)],[k*(self.y2-self.y1)]]) # 2x1 
+u2 = np.array([[ k*(self.x1-self.x2)],[k*(self.y1-self.y2)]]) # 2x1
+        
 " Calculate V1/W1 and V2/W2 "
-Speed_G1 = np.array([[v1], [w1]])
-Speed_G2 = np.array([[v2], [w2]])
-Speed_G1 = np.array([[1,0], [0, 1/L]])*np.array([[math.cos(Theta1), math.sin(Theta1)], [-math.sin(Theta1), math.cos(Theta1)]])*u1
-Speed_G2 = np.array([[1,0], [0, 1/L]])*np.array([[math.cos(Theta2), math.sin(Theta2)], [-math.sin(Theta2), math.cos(Theta2)]])*u2
-
+            
+S1 = np.array([[self.v1], [self.w1]]) #2x1
+G1 = np.array([[1,0], [0,1/L]]) #2x2
+F1 = np.array([[math.cos(self.Theta1),math.sin(self.Theta1)],[-math.sin(self.Theta1),math.cos(self.Theta1)]]) #2x2
+S1 = np.dot(np.dot(G1, F1), u1) #2x1
+        
+           
+S2 = np.array([[self.v2], [self.w2]]) #2x1
+G2 = np.array([[1,0], [0,1/L]]) #2x2
+F2 = np.array([[math.cos(self.Theta2),math.sin(self.Theta2)],[-math.sin(self.Theta2),math.cos(self.Theta2)]]) #2x2
+S2 = np.dot(np.dot(G2, F2), u2) # 2x1
+            
 " Calculate VL1/VR1 and VL2/VR2 "
-D = np.array([[ 1/2, 1/2], [-1/(2*d), 1/(2*d) ]])
-Di = np.linalg.inv(D)
-Speed_L1 = np.array([[vL1], [vR1]]) 
-Speed_L2 = np.array([[vL2], [vR2]])
-Speed_L1 = Di*np.array([[v1], [w1]])
-Speed_L2 = Di*np.array([[v2], [w2]])
+            
+D = np.array([[1/2,1/2],[-1/(2*d),1/(2*d)]]) #2x2
+Di = np.linalg.inv(D) #2x2
+        
+    
+    
+Speed_L1 = np.array([[self.vL1], [self.vR1]]) # Vector 2x1 for Speed of Robot 1
+Speed_L2 = np.array([[self.vL2], [self.vR2]]) # Vector 2x1 for Speed of Robot 2 
+M1 = np.array([[S1[0]],[S1[1]]]).reshape(2,1) #2x1
+M2 = np.array([[S2[0]], [S2[1]]]).reshape(2,1) #2x1
+Speed_L1 = np.dot(Di, M1) # 2x1 (VL1, VR1)
+Speed_L2 = np.dot(Di, M2) # 2x1 (VL2, VR2)
+            
+VL1 = float(Speed_L1[0])
+VR1 = float(Speed_L1[1])
+VL2 = float(Speed_L2[0])
+VR2 = float(Speed_L2[1])
