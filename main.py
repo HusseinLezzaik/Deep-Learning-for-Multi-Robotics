9ea4f6602cb0d4acea5c462d00ba9c83af5a86fc
hussein <hsenlezaik@gmail.com>
2021-03-29 13:35:22 +0200

    Main edit

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ k = 1 # Control Gain
 L = 1
 d = 0.5
 distance = 6
+count = 2
 def euler_from_quaternion(x, y, z, w):
         
      t3 = +2.0 * (w * z + x * y)
@@ -122,6 +123,7 @@ class MinimalPublisher(Node):
         VL2 = float(Speed_L2[0])
         VR2 = float(Speed_L2[1])
         
+        count = 2
         distance = self.x2 - self.x1
         print(distance) 
         
@@ -141,6 +143,7 @@ class MinimalPublisher(Node):
         
         
         " Write Values to CSV1 and CSV2 "
+        
         if distance > 0.2:
             
             with open('robot1.csv', 'a', newline='') as f:
@@ -162,8 +165,9 @@ class MinimalPublisher(Node):
                     self.i2 = 1
             
                 thewriter.writerow({'Data_X' : self.X2, 'Data_Y' : self.Y2, 'Label_X' : self.U2[0], 'Label_Y' : self.U2[1]})
-                
-        
+               
+              
+            
         " Speed Commands to Robot 1"
         
         msgl1 = Float32()    
