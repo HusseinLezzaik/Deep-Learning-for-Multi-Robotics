dc840a0d04f390824bb674fa93d4ef809df6edd3
hussein <hsenlezaik@gmail.com>
2021-03-29 16:47:23 +0200

    Add delta time

--- main.py
+++ main.py
@@ -159,20 +159,18 @@ class MinimalPublisher(Node):
                 
                     thewriter.writerow({'Data_X' : self.X1, 'Data_Y' : self.Y1, 'Label_X' : self.U1[0], 'Label_Y' : self.U1[1]})
                 
-            with open('robot2.csv', 'a', newline='') as f:
-                fieldnames = ['Data_X', 'Data_Y', 'Label_X', 'Label_Y']
-                thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+                with open('robot2.csv', 'a', newline='') as f:
+                    fieldnames = ['Data_X', 'Data_Y', 'Label_X', 'Label_Y']
+                    thewriter = csv.DictWriter(f, fieldnames=fieldnames)
             
-                if self.i2 == 0:
-                    thewriter.writeheader()
-                    self.i2 = 1
+                    if self.i2 == 0:
+                        thewriter.writeheader()
+                        self.i2 = 1
             
-                thewriter.writerow({'Data_X' : self.X2, 'Data_Y' : self.Y2, 'Label_X' : self.U2[0], 'Label_Y' : self.U2[1]})
+                    thewriter.writerow({'Data_X' : self.X2, 'Data_Y' : self.Y2, 'Label_X' : self.U2[0], 'Label_Y' : self.U2[1]})
                
         self.count += 1
-        #print(self.count % 2 == 0)            
-       
-        #print(self.count % 2 == 0)   
+        
         " Speed Commands to Robot 1"
         
         msgl1 = Float32()    
