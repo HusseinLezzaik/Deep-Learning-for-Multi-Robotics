42e42f7632819af6bff9c4bc3e278af89b3114d8
hussein <hsenlezaik@gmail.com>
2021-03-29 16:44:21 +0200

    Add delta time

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ class MinimalPublisher(Node):
         " Write Values to CSV1 and CSV2 "
         
         if distance > 0.2:
-            if self.count % 2 == 0:
+            if self.count % 1 == 0:
                 
                 with open('robot1.csv', 'a', newline='') as f:
                     fieldnames = ['Data_X', 'Data_Y', 'Label_X', 'Label_Y']
@@ -169,7 +169,7 @@ class MinimalPublisher(Node):
             
                 thewriter.writerow({'Data_X' : self.X2, 'Data_Y' : self.Y2, 'Label_X' : self.U2[0], 'Label_Y' : self.U2[1]})
                
-        self.count += 0.5
+        self.count += 1
         #print(self.count % 2 == 0)            
        
         #print(self.count % 2 == 0)   
