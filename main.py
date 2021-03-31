40c1560cdbd1b1a4515d3f11156cddc738f9b41d
hussein <hsenlezaik@gmail.com>
2021-03-31 12:35:36 +0200

    edit main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""
+ """
 
 Publisher node speed of a single Mobile Robot Wheel
 
@@ -152,24 +152,24 @@ class MinimalPublisher(Node):
             if self.count % 2 == 0:
                 
                 with open('robot1.csv', 'a', newline='') as f:
-                    fieldnames = ['Data_X', 'Data_Y', 'Label_X', 'Label_Y']
+                    fieldnames = ['Data_X', 'Data_Y', 'Angle', 'Label_X', 'Label_Y']
                     thewriter = csv.DictWriter(f, fieldnames=fieldnames)
                 
                     if self.i1 == 0:
                         thewriter.writeheader()
                         self.i1 = 1
                 
-                    thewriter.writerow({'Data_X' : self.X1, 'Data_Y' : self.Y1, 'Label_X' : self.U1[0][0], 'Label_Y' : self.U1[1][0]})
+                    thewriter.writerow({'Data_X' : self.X1, 'Data_Y' : self.Y1, 'Angle' : self.Theta1, 'Label_X' : self.U1[0][0], 'Label_Y' : self.U1[1][0]})
                 
                 with open('robot2.csv', 'a', newline='') as f:
-                    fieldnames = ['Data_X', 'Data_Y', 'Label_X', 'Label_Y']
+                    fieldnames = ['Data_X', 'Data_Y', 'Angle', 'Label_X', 'Label_Y']
                     thewriter = csv.DictWriter(f, fieldnames=fieldnames)
             
                     if self.i2 == 0:
                         thewriter.writeheader()
                         self.i2 = 1
             
-                    thewriter.writerow({'Data_X' : self.X2, 'Data_Y' : self.Y2, 'Label_X' : self.U2[0][0], 'Label_Y' : self.U2[1][0]})
+                    thewriter.writerow({'Data_X' : self.X2, 'Data_Y' : self.Y2, 'Angle' : self.Theta2, 'Label_X' : self.U2[0][0], 'Label_Y' : self.U2[1][0]})
                
         self.count += 1
         
