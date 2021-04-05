e23fc4156c6694a93ccb338380d51984cb3c7e78
hussein <hsenlezaik@gmail.com>
2021-04-05 22:41:19 +0200

    edit

---
+++ Python Remote API/initialpositioncontrol.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Control Initial Position of two robots using Python Remote API 
+
+@author: hussein
+"""
+
+import sim
+import time 
+import math
+print("Program Started")
+
+
