68f8b424b5535badbc565e70c2b3dfc9d4792ec3
hussein <hsenlezaik@gmail.com>
2021-04-16 08:39:25 +0200

    add graph neural nets

---
+++ Graph Neural Nets/data_loader1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 08:26:41 2021
+
+@author: hussein
+"""
+
---
+++ Graph Neural Nets/two_layer_gnn1.py
@@ -0,0 +1,7 @@
+"""
+Building GNN for robot1: 
+    
+*Input: Local Relative pose of robot2
+*Output: Local Control Input for robot1
+"""
+
