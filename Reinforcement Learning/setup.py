8a75e5fc3b0da8900ba991e1e852e9b37607d5e4
hussein <hsenlezaik@gmail.com>
2021-06-14 23:52:22 +0200

    create custom env for CoppeliaSim

--- Reinforcement Learning/setup.py
+++ Reinforcement Learning/setup.py
@@ -5,9 +5,26 @@ Setup file for gym and v-rep
 """
 # What you enter as the name variable of the setup is what you will use to import your environment(for eg. here, import gym_vrep).
 
-from setuptools import setup
+# from setuptools import setup
 
-setup(name='gym_vrep',
-      version='0.0.1',
-      install_requires=['gym']#And any other dependencies required
+# setup(name='gym_vrep',
+#       version='0.0.1',
+#       install_requires=['gym']#And any other dependencies required
+# )
+
+from setuptools import setup, find_packages
+
+install_requires=[
+    'gym',
+    'numpy'
+]
+
+setup(name='vrep_env',
+      version='0.0.2',
+      description='V-REP integrated with OpenAI Gym',
+      url='https://github.com/ycps/vrep-env',
+      packages=[package for package in find_packages() if package.startswith('vrep_env')],
+      install_requires=install_requires,
+      package_data={'': ['remoteApi.so']},
+      include_package_data=True
 )
